be29015675e25dec267d7a32604e4742306fec30
herz <herzblutnord@proton.me>
2025-12-30 13:03:51 +0100

    notif fix 2000+1

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -12,18 +12,12 @@ plugins {
 }
 
 dependencies {
-    // Import the Firebase BoM
     implementation(platform("com.google.firebase:firebase-bom:34.7.0"))
-
-
-    // TODO: Add the dependencies for Firebase products you want to use
-    // When using the BoM, don't specify versions in Firebase dependencies
     implementation("com.google.firebase:firebase-analytics")
 
-
-    // Add the dependencies for any other desired Firebase products
-    // https://firebase.google.com/docs/android/setup#available-libraries
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.5")
 }
+
 
 // ---- Release signing (reads android/key.properties) ----
 val keystoreProperties = Properties()
@@ -40,6 +34,7 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
+        isCoreLibraryDesugaringEnabled = true
     }
 
     kotlinOptions {
